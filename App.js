bb55cb9093abf5b8a9b9cde2b7cba808fd5176ba
mhmtyildiz <97394946+mhmtyildiz@users.noreply.github.com>
2024-08-22 02:49:28 +0300

    App.js dosyası düzenlendi ve syntax hatası giderildi.

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Navigator, StackScreen, NavigationProvider } from '@react-navigation/native';
+import { NavigationContariner } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 
-const App = () => (
-  <NavigationProvider screenName="LoginScreen">
-    <Navigator initialRoute={() => {
-      return { screen: "LoginScreen" };
-    }}/>
-  </NavigationProvider>
-};
+const Stack = createStackNavigator();
 
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator initialRoute={ () => <LoginScreen /> } />
+  </NavigationContainer>
+);
 
 export default App;
