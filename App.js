bccf10b159ff10a27b92de7ec821e14b2e33815f
mhmtyildiz <97394946+mhmtyildiz@users.noreply.github.com>
2024-08-22 02:47:55 +0300

    App.js dosyası tekrar düzeltildi.

--- App.js
+++ App.js
@@ -1,2 +1,14 @@
 import React from 'react';
-import { Navigator, StackFFVfVÇB° 
+import { Navigator, StackScreen, NavigationProvider } from '@react-navigation/native';
+import LoginScreen from './src/screens/LoginScreen';
+
+const App = () => (
+  <NavigationProvider screenName="LoginScreen">
+    <Navigator initialRoute={() => {
+      return { screen: "LoginScreen" };
+    }}/>
+  </NavigationProvider>
+};
+
+
+export default App;
