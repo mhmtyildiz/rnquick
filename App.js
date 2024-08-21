d4f17c61378db2365918b463d23e5c3585f21cf0
mhmtyildiz <97394946+mhmtyildiz@users.noreply.github.com>
2024-08-22 02:54:42 +0300

    App.js dosyası düzenlendi ve eksik içerik eklendi.

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigition/stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createStackNavigator();
 
 const App = () => (
   <NavigationContainer>
-    <Stack.Navigator initialRoute={ () => <LoginScreen /> } />
+    <Stack.Navigator initialRouteName="LoginScreen">
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    </Stack.Navigator>
   </NavigationContainer>
-
 );
 
 export default App;
